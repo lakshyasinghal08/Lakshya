02a30fa8cbb9230dd07012ff72e5b3226123dcfd
Lakshya Singhal <lakshyasinghal92@gmail.com>
2025-12-12 15:36:26 +0530

    practice

--- pointer.cpp
+++ pointer.cpp
@@ -90,13 +90,126 @@ using namespace std;
 //     return 0;
 // }
 
-int main(){
-    int arr[]= {10,20,30,40};
+// int main(){
+//     int arr[]= {10,20,30,40};
 
-    int *ptr = arr;
-    cout<<*(ptr + 1)<<endl;
-    cout<<*(ptr + 3)<<endl;
-    ptr++;
-    cout<<*ptr<<endl;
-    return 0;
+//     int *ptr = arr;
+//     cout<<*(ptr + 1)<<endl;
+//     cout<<*(ptr + 3)<<endl;
+//     ptr++;
+//     cout<<*ptr<<endl;
+//     return 0;
+// }
+
+ /*Declare an integer variable a and a pointer p that stores the address of a.
+Then print the value of a using the pointer.*/
+// int main(){
+//     int a = 10;
+//     int* ptr = &a;
+//     cout<<*ptr<<endl;
+
+//     return 0;
+// }
+
+/*Write a program to change the value of a variable from 10 to 50 using a pointer.*/
+// void changevalue(int *a){
+//     *a = 50;
+// }
+// int main(){
+//     int a = 10;
+
+//     changevalue(&a);
+//     cout<<"a = "<< a <<endl;
+
+//     return 0;
+// }
+// int main(){
+//     int arr[] = {10,20,30};
+//     int* ptr = arr;
+    
+//     cout<<*(ptr+1)<<endl;
+
+//     return 0;
+// }
+
+// int main(){
+//     int a = 25;
+//     int* ptr = &a;
+
+//     cout<<&a<<endl;
+//     cout<<ptr<<endl;
+
+//     return 0;
+// }
+
+// void update(int *p){
+//     *p = 100;
+// }
+// int main(){
+//     int a = 30;
+//     update(&a);
+
+//     cout<<a<<endl;
+
+//     return 0;
+// }
+
+// int main(){
+//     int x = 20;
+//     int *ptr = &x;
+
+//     (*ptr)++;
+//     cout<<x<<endl;
+//     cout<<*(ptr)<<endl;
+
+//     return 0;
+// }
+
+// void swap(int *a , int *b){
+// int temp;
+// temp = *a;
+// *a = *b;
+// *b = temp;
+// }
+
+// int main(){
+// int x, y;
+// cout<<"enter the value of x \n";
+// cin>>x;
+// cout<<"enter the value of y \n";
+// cin>>y;
+// cout<<"before swapping :\n"<<endl;
+// cout<<"x = "<<x<<endl;
+// cout<<"y = "<<y<<endl;
+// swap(&x , &y);
+// cout<<"After swapping : \n"<<endl;
+// cout<<"x = "<<x<<endl;
+// cout<<"y = "<<y<<endl;
+
+// return 0;
+// }
+
+// int main(){
+// int a = 5;
+// int *p = &a;
+// int **q = &p;
+
+// cout<<**q<<endl;
+
+// return 0;
+// }
+
+// int main(){
+//     int arr[] = {2, 4, 6, 8};
+//     int* ptr = arr;
+
+//     cout<<*(ptr+2)<<endl;
+
+//     return 0;
+// }
+
+int main(){
+    int *ptr = NULL;
+    cout<<ptr<<endl;
+    return 0;   
 }
