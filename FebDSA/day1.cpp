0d5dee5519d8b769948d60f92e6b8e53a96695c0
Lakshya Singhal <lakshyasinghal92@gmail.com>
2026-02-01 17:46:29 +0530

    start DSA consistent

---
+++ FebDSA/day1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<"lakshya singhal"<<endl;
+    // int a = 18;
+    // double b = 3.9;
+    // char d = 'l';
+    // cout<<"a ="<<a<<endl<<"b ="<<b<<endl<<"d ="<<d<<endl;
+    // cout<<sizeof(a)<<endl;
+    // cout<<sizeof(b)<<endl;
+    // cout<<sizeof(d)<<endl;
+     
+    // type casting
+    // int a = 'a';
+    // cout<<a<<endl;
+
+    // int b = -12;
+    // cout<<"b = " << b << endl;
+    // char ch;
+    // cout<<"enter the character : ";
+    // cin>>ch;
+
+    // if(ch>='a' && ch<='z'){
+    //     cout<<"character is a lowercase : ";
+    // }
+    // else if(ch>='A' && ch<='Z'){
+    //     cout<<"character is an upper case: ";
+    //     }
+    // else if(ch>='0' && ch<='9'){
+    //     cout<<"character is an integer : ";
+    // }
+    // else{
+    //     cout<<"you can enter the invalid number : try again !!";
+    // }
+    int n;
+    cout<<"enter the value of n :";
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+    cout<<i<<endl;
+    i++;
+    }
+    return 0;
+}
