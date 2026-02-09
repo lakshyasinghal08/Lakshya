7fd7da8dfb3e74d677a5b48d2bdfe209fc0193da
Lakshya Singhal <lakshyasinghal92@gmail.com>
2026-02-09 07:55:37 +0530

    morre and datch algo

---
+++ FebDSA/day5.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<set>
+#include<vector>
+using namespace std;
+int algo(int b[] , int size){
+// for(int i=0; i<size; i++){
+//     int count = 0;
+//     for(int j=0; j<size;j++){
+//         if(b[i]==b[j]){
+//             count++;
+//         }
+//     }
+//     if(count > size/2){
+//         return b[i];
+//     }
+// }
+// return -1;
+map<int ,int>mpp;
+for(int i=0; i<size; i++){
+    mpp[b[i]]++;
+}
+for(auto it:mpp){
+    if(it.second>(size/2) ){
+      return it.first;
+    }
+}
+return -1;
+}
+int main(){
+//    int a[] = {1,2,0,0,1,2,0,1,1,0,2,2,0,2,1};
+//    int n  = sizeof(a)/sizeof(int);
+
+//    int count_0 = 0;
+//    int count_1 = 0;
+//    int count_2 = 0;
+//    for(int i = 0; i<n; i++){
+//     if(a[i] == 0){
+//         count_0++;
+//     }
+//     else if(a[i] == 1){
+//         count_1++;
+//     }
+//     else{
+//         count_2++;
+//     }
+//    }
+// for(int i=0; i<count_0; i++){
+//      a[i] = 0;
+// }
+// for(int i=count_0; i<count_0+count_1;i++){
+//     a[i] = 1;
+// }
+// for(int i = count_0+count_1; i<n; i++){
+//     a[i] =2;
+// }
+// for(int i=0; i<n; i++){
+//     cout<<a[i]<<" ";
+// }
+
+// int low = 0;
+// int mid = 0;
+// int high = n-1;
+// while(mid <= high){
+//     if(a[mid] == 0){
+//         swap(a[low] , a[mid]);
+//         low++;
+//         mid++;
+//     }
+//     else if(a[mid] == 1){
+//         mid++;
+//     }
+//     else {
+//         swap(a[mid] , a[high]);
+//         high--;
+//     }   
+// }
+//  for(int i = 0; i < n; i++){
+//         cout << a[i] << " ";
+//     }
+
+int b[] = {2,2,1,3,2 , 4,2,2};
+int size = sizeof(b)/sizeof(int);
+
+// int array = algo(b,size);
+// cout<<"array = "<<array<<" "<<endl;
+
+int count = 0;
+int el;
+for(int i=0; i<size; i++)
+{
+    if(count == 0){
+        count++;
+        el = b[i];
+    }
+    else if(b[i] == el){
+        count++;
+    }
+    else{
+        count--;
+    }
+}
+ int count1 = 0;
+    for(int i=0; i<size; i++){
+        if(b[i] == el){
+            count1++;
+        }
+    }
+    if(count1>(size/2)){
+        cout<<el<<"is the highest occuring element : "<<endl;
+    }
+    else{
+        cout<<"no majority element : "<<endl;
+    }
+    return 0;
+}
