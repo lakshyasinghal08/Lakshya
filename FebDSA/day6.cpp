65c7a585c82763f15c5c269b137515f4a5cd1029
Lakshya Singhal <lakshyasinghal92@gmail.com>
2026-02-10 07:51:39 +0530

    rearrange the array

---
+++ FebDSA/day6.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<set>
+#include<vector>
+using namespace std;
+
+int main(){
+    // int a[] = {-2,-3,4,-1,-2,1,5,-3};
+    // int a[] = {3,1,-2,-5,2,-4};
+    // int n = sizeof(a)/sizeof(int);
+    // int maxi = LONG_MIN;
+    // for(int i=0; i<n;i++){
+    //     for(int j=i; j<n; j++){
+    //         int sum = 0;
+    //         for(int k=i;k<j;k++){
+    //             sum +=a[k];
+    //         }
+    //         maxi = max(maxi , sum);
+    //     }
+    // }
+    //     cout<<"maximum subarray sum is : "<<maxi<<" ";
+    
+    // for(int i=0; i<n;i++){
+    //     int sum = 0;
+    //     for(int j=i;j<n;j++){
+    //         sum += a[j];
+    //         maxi = max(maxi , sum);
+    //     }
+    // }
+    //   cout<<"maximum subarray sum is : "<<maxi<<" ";
+//    int sum  = 0;
+//    int ansstart ;
+//    int ansend = 0;
+//    for(int i=0; i<n; i++){
+//         sum += a[i];
+//         ansstart = 
+//         maxi = max(maxi , sum);
+    
+//     if(sum < 0){
+//         sum = 0;
+//     }
+//    }
+//    cout<<"maximum subarray sum = "<<maxi<<" ";
+
+// vector<int>pos;
+// vector<int>neg;
+// for(int i=0; i<n;i++){
+//     if(a[i]>=0){
+//         pos.push_back(a[i]);
+//     }
+//     else{
+//         neg.push_back(a[i]);
+//     }
+// }
+// // TC = O(2n) and SC = O(n)
+// for(int i=0; i<n/2; i++){
+//     a[2*i] = pos[i];
+//     a[2*i+1] = neg[i];
+// }
+// for(int i=0;i<n; i++){
+// cout<<a[i]<<" ";
+// }
+
+
+// TC = O(n) and SC = O(n)
+// vector<int>ans(n,0);
+// int negindex = 1;
+// int posindex = 0;
+// for(int i=0; i<n; i++){
+//     if(a[i] < 0){
+//         ans[negindex] = a[i];
+//         negindex += 2;
+//     }
+//     else{
+//         ans[posindex] = a[i];
+//         posindex += 2;
+//     }
+// }
+// for(int i=0; i<n; i++){
+//     cout<<ans[i]<<" ";
+// }
+
+int a[] = {3,1,-2,-5,2,8,9,10-4};
+    int n = sizeof(a)/sizeof(int);
+    int maxi = LONG_MIN;
+
+    vector<int>pos,neg;
+    for(int i=0; i<n; i++){
+        if(a[i] > 0){
+            pos.push_back(a[i]);
+        }
+        else{
+            neg.push_back(a[i]);
+        }
+    }
+    if(pos.size() > neg.size()){
+        for(int i=0; i<neg.size();i++){
+            a[2*i] = pos[i];
+            a[2*i+1] = neg[i];
+        }
+        int index = neg.size() * 2;
+        for(int i=neg.size();i<pos.size(); i++){
+            a[index] = pos[i];
+            index++;
+        }
+    }
+    else{
+        for(int i=0; i<pos.size();i++){
+            a[2*i] = pos[i];
+            a[2*i+1] = neg[i];
+        }
+        int index = pos.size() * 2;
+        for(int i=pos.size();i<neg.size(); i++){
+            a[index] = neg[i];
+            index++;
+        }
+    }
+    for(int i=0; i<n; i++){
+        cout<<a[i] << " ";
+    }
+    return 0;
+}
