ac1e2b4afd4e932fb03fa89741983852be9c191b
Lakshya Singhal <lakshyasinghal92@gmail.com>
2026-02-11 21:10:45 +0530

    revision

--- FebDSA/day7.cpp
+++ FebDSA/day7.cpp
@@ -42,10 +42,10 @@ int main(){
 //         cout<<a[i]<<" ";
 //     }
 
-int a[] = {10,12,22,12,0,3,6};
-int n = sizeof(a)/sizeof(int);
-vector<int>ans;
-int maxi = INT_MIN;
+// int a[] = {10,12,22,12,0,3,6};
+// int n = sizeof(a)/sizeof(int);
+// vector<int>ans;
+// int maxi = INT_MIN;
 // for(int i=0; i<n;i++){
 //     bool leader = true;
 //     for(int j=i+1;j<n;j++){
@@ -61,16 +61,78 @@ int maxi = INT_MIN;
 // for(auto x : ans){
 //     cout<<x<<" ";
 // }
-for(int i=n-1;i>=0;i--){
-    if(a[i] > maxi){
-        ans.push_back(a[i]);
+// for(int i=n-1;i>=0;i--){
+//     if(a[i] > maxi){
+//         ans.push_back(a[i]);
+//     }
+//     maxi = max(maxi , a[i]);
+// }
+// sort(ans.begin() , ans.end());
+// for(auto x:ans){
+//     cout<<x<<" ";
+// }
+
+// int b[] = {2,1,3,5,6,7,4};
+// int b[] = {2,1,5,4,3,0,0};
+// int n = sizeof(b)/sizeof(int);
+// int mini = b[0];
+// int profit = 0;
+// for(int i=0; i<n; i++){
+//     int cost = b[i] - mini;
+//     profit = max(profit , cost);
+//     mini = min(mini  , b[i]);
+// }
+// cout<<"the profit = "<<profit<<" ";
+
+// int index = -1;
+// for(int i=n-2;i>=0;i--){
+//     if(b[i] < b[i+1]){
+//         index = i;
+//         break;
+//     }}
+//     if(index == -1){
+//         reverse(b,b+n);
+//     }
+// else{
+// for(int i=n-1;i>index; i--){
+//     if(b[i] > b[index]){
+//        swap(b[i] , b[index]);
+//        break;
+//     }
+// }
+// reverse(b+ index+1 , b+n);
+// }
+// for(int i=0; i<n; i++){
+//     cout<<b[i]<<" ";
+// }
+
+int c[] = {10,12,22,12,0,3,6};
+int n = sizeof(c)/sizeof(int);
+vector<int>ans;
+int maxi = INT_MIN;
+// for(int i=0;i<n;i++){
+//     bool leader = true;
+//     for(int j = i+1; j<n;j++){
+//         if(c[j] > c[i]){
+//             leader = false;
+//         }
+//     }
+//     if(leader){ 
+//         ans.push_back(c[i]);
+//     }
+// }
+// for(auto x : ans){
+//     cout<<x<<" ";
+// }
+for(int i=n-1; i>=0;i--){
+    if(c[i] > maxi){   
+        ans.push_back(c[i]);
     }
-    maxi = max(maxi , a[i]);
+    maxi = max(maxi , c[i]); 
 }
 sort(ans.begin() , ans.end());
 for(auto x:ans){
     cout<<x<<" ";
 }
-
     return 0;
 }
